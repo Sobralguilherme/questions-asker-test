d6bce58da781bbb6924b9374805dff986b503b2d
Sobralguilherme <151589379+Sobralguilherme@users.noreply.github.com>
2023-12-11 19:11:51 -0300

    Update settings.py databese add
    add a update in bd type or sql 3 or postregss incliude in # and database config.

--- meu_projeto/meu_projeto_django/meu_projeto_dockers3/settings.py
+++ meu_projeto/meu_projeto_django/meu_projeto_dockers3/settings.py
@@ -76,7 +76,15 @@ WSGI_APPLICATION = "meu_projeto_django.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
+       # OR 'ENGINE': 'django.db.backends.postgresql',
         "NAME": BASE_DIR / "db.sqlite3",
+        'USER': config('DB_USER', default='yourdbuser'),
+        'PASSWORD': config('DB_PASSWORD', default='yourdbpassword'),
+        'HOST': config('DB_HOST', default='db'),
+        'PORT': config('DB_PORT', default='5432'),
+    }
+}
+
     }
 }
 
